a4e31d034846c5340c0ee519b08438df7a65a267
byped <gmbabbitt@gmail.com>
2014-01-15 09:45:31 -0500

    updating respond.js to 1.4.2

--- vendor/assets/javascripts/respond.proxy.js
+++ vendor/assets/javascripts/respond.proxy.js
@@ -3,14 +3,19 @@
 	var docElem			= doc.documentElement,
 		proxyURL		= doc.getElementById("respond-proxy").href,
 		redirectURL		= (doc.getElementById("respond-redirect") || location).href,
+		baseElem		= doc.getElementsByTagName("base")[0],
 		urls			= [],
 		refNode;
+
+	function encode(url){
+		return win.encodeURIComponent(url);
+	}
 
 	 function fakejax( url, callback ){
 
 		var iframe,
 			AXO;
-		
+
 		// All hail Google http://j.mp/iKMI19
 		// Behold, an iframe proxy without annoying clicky noises.
 		if ( "ActiveXObject" in win ) {
@@ -25,8 +30,8 @@
 			docElem.insertBefore(iframe, docElem.firstElementChild || docElem.firstChild );
 		}
 
-		iframe.src = proxyURL + "?url=" + redirectURL + "&css=" + url;
-		
+		iframe.src = checkBaseURL(proxyURL) + "?url=" + encode(redirectURL) + "&css=" + encode(checkBaseURL(url));
+
 		function checkFrameName() {
 			var cssText;
 
@@ -41,7 +46,7 @@
 				iframe.parentNode.removeChild(iframe);
 				iframe = null;
 
-			
+
 				// Per http://j.mp/kn9EPh, not taking any chances. Flushing the ActiveXObject
 				if (AXO) {
 					AXO = null;
@@ -57,17 +62,25 @@
 				win.setTimeout(checkFrameName, 100);
 			}
 		}
-		
+
 		win.setTimeout(checkFrameName, 500);
 	}
-	
+
+	function checkBaseURL(href) {
+		if (baseElem && href.indexOf(baseElem.href) === -1) {
+			bref = (/\/$/).test(baseElem.href) ? baseElem.href : (baseElem.href + "/");
+			href = bref + href;
+		}
+
+		return href;
+	}
+
 	function checkRedirectURL() {
 		// IE6 & IE7 don't build out absolute urls in <link /> attributes.
 		// So respond.proxy.gif remains relative instead of http://example.com/respond.proxy.gif.
 		// This trickery resolves that issue.
 		if (~ !redirectURL.indexOf(location.host)) {
 
-			// Inject an <a> attribute, with the redirectURL
 			var fakeLink = doc.createElement("div");
 
 			fakeLink.innerHTML = '<a href="' + redirectURL + '"></a>';
@@ -81,30 +94,31 @@
 			fakeLink = null;
 		}
 	}
-	
+
 	function buildUrls(){
 		var links = doc.getElementsByTagName( "link" );
-		
+
 		for( var i = 0, linkl = links.length; i < linkl; i++ ){
-			
+
 			var thislink	= links[i],
 				href		= links[i].href,
-				ext			= /^([a-zA-Z]+?:(\/\/)?(www\.)?)/;
+				extreg		= (/^([a-zA-Z:]*\/\/(www\.)?)/).test( href ),
+				ext			= (baseElem && !extreg) || extreg;
 
 			//make sure it's an external stylesheet
-			if( thislink.rel.indexOf( "stylesheet" ) >= 0 && ext.test( href ) ){
-				(function( link ){			
+			if( thislink.rel.indexOf( "stylesheet" ) >= 0 && ext ){
+				(function( link ){
 					fakejax( href, function( css ){
 						link.styleSheet.rawCssText = css;
 						respond.update();
 					} );
 				})( thislink );
-			}	
+			}
 		}
 
-		
+
 	}
-	
+
 	if( !respond.mediaQueriesSupported ){
 		checkRedirectURL();
 		buildUrls();
